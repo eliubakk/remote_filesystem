3f8b61af87a7bd3a7a3ab8ba8c1bcb1f2a0bc6c5
Nick Weber <webenic3@gmail.com>
2018-03-30 20:58:09 -0400

    Fixed test case error, my bad

--- test_fs_session.cc
+++ test_fs_session.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cstdlib>
-#include "fs_client.h"
 #include "fs_server.h"
 
 using namespace std;
@@ -11,18 +10,14 @@ int main(int argc, char *argv[]){
     unsigned int session = 6, seq = 0;
 
     if (argc != 3) {
-        cout_lock.lock();
         cout << "error: usage: " << argv[0] << " <server> <serverPort>\n";
-        cout_lock.unlock();
         exit(1);
     }
     server = argv[1];
     server_port = atoi(argv[2]);
 
     fs_clientinit(server, server_port);
-    cout_lock.lock();
     cout << "PRE_SESSION: " << session << endl;
     cout << "return val: " << fs_session("user1", "password1", &session, seq++) << endl;
     cout << "POST_SESSION: " << session << endl;
-    cout_lock.unlock();
 }
